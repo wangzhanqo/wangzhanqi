dbf341adde7dd1d28ca2fe3440d1a2b1a9d67b40
wangzhanqi <6466687@qq.com>
2020-01-18 10:45:30 +0800

    redis

---
+++ untitled/src/main/java/Three.java
@@ -0,0 +1,25 @@
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Transaction;
+
+import java.util.Set;
+
+public class Three {
+    public static void main(String[] args) {
+        Jedis jedis=new Jedis("127.0.0.1",6379);
+        //jedis.set("money","111");
+        //jedis.expire("money",999);
+       //System.out.println(jedis.ping());
+        // System.out.println(jedis);
+//        jedis.set("wang","250");
+////        jedis.watch("wang");
+////        jedis.set("wang","9999");
+////        jedis.unwatch();
+////        jedis.watch("wang");
+////        jedis.multi();
+////        jedis.set("wang","10000");
+        Transaction transaction=jedis.multi();
+        transaction.set("456","v456");
+
+        transaction.exec();
+    }
+}
